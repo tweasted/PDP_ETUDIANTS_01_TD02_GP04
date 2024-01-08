80728e282e519cd5425b7092092557d56397255c
Isabellebrcd1 <isabelle.bricaud@hotmail.fr>
2024-01-08 22:12:57 +0100

    e3

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,9 @@
 // Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
 // Pin 15 can work but DHT must be disconnected during program upload.
 
+#define DEEP_SLEEP 5
+#define uS_to_S 1000000 
+
 // Uncomment the type of sensor in use:
 #define DHTTYPE    DHT11     // DHT 11
 //#define DHTTYPE    DHT22     // DHT 22 (AM2302)
@@ -45,13 +48,7 @@ void setup() {
   Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
   Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
   Serial.println(F("------------------------------------"));
-  // Set delay between sensor readings based on sensor details.
-  delayMS = sensor.min_delay / 1000;
-}
-
-void loop() {
-  // Delay between measurements.
-  delay(delayMS);
+  
   // Get temperature event and print its value.
   sensors_event_t event;
   dht.temperature().getEvent(&event);
@@ -73,4 +70,19 @@ void loop() {
     Serial.print(event.relative_humidity);
     Serial.println(F("%"));
   }
+
+  
+  
+  // Enter deep sleep mode for 5 seconds
+  esp_sleep_enable_timer_wakeup(DEEP_SLEEP * uS_to_S); 
+  Serial.println(F("Entering in deep sleep mode for 5 s.")); 
+  Serial.flush();
+  esp_deep_sleep_start();  
+  Serial.println(F("test")); // n'est jamais print donc good et 5 sec entre les print plus haut 
+
+}
+
+
+void loop() {
+  
 }
